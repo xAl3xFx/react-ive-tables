dab725e85c0b300ddc698268ba4ec1c5da4330b8
Aleksandar Bakalov <axelgron@gmail.com>
2022-03-25 12:54:47 +0100

    Fixed HeaderButton import

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 import {LazyDataTable} from './lib/LazyDataTable'
 import { SimpleDataTable } from './lib/SimpleDataTable'
 import { SimpleTreeTable } from './lib/SimpleTreeTable'
-import { HeaderButton } from './lib/types'
+import {HeaderButton} from "./types";
 export {LazyDataTable, SimpleDataTable, SimpleTreeTable, HeaderButton}
