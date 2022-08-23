6b8f49f423d5b6f6165cf616de17aaeb103f6a7a
Alex Petrov <alex.petrov@inerps.eu>
2022-08-23 23:01:58 +0300

    Exported HeaderButton from index.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {SimpleDataTable} from "./SimpleDataTable";
 import {LazyDataTable} from "./LazyDataTable";
 import {SimpleTreeTable} from "./SimpleTreeTable";
+import {HeaderButton} from '../types'
 
-export {SimpleDataTable, LazyDataTable, SimpleTreeTable}
+export {SimpleDataTable, LazyDataTable, SimpleTreeTable, HeaderButton}
