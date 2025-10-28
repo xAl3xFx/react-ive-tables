564ca9db93f58801f81279aea1cc9b5755bc92e0
Aleksandar Bakalov <axelgron@gmail.com>
2025-10-28 16:31:48 +0200

    Removed LazyDataTable.tsx

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { ReactiveTable, FetchDataParams, ExportConfig } from './ReactiveTable';
 export { SimpleDataTable } from './SimpleDataTable';
-export { LazyDataTable } from './LazyDataTable';
 export { SimpleTreeTable } from './SimpleTreeTable';
 export { HeaderButton } from './types';
