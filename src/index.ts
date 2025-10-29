4e091835f43fb3c35e4404dad10457ffc7f72442
Aleksandar Bakalov <axelgron@gmail.com>
2025-10-29 11:01:54 +0200

    Some adjustments

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { ReactiveTable, FetchDataParams, ExportConfig } from './ReactiveTable';
 export { SimpleDataTable } from './SimpleDataTable';
 export { SimpleTreeTable } from './SimpleTreeTable';
+export { MobileFilters } from "./mobile/MobileFilters"
+export { MobileDataView } from "./mobile/MobileDataView"
 export { HeaderButton } from './types';
