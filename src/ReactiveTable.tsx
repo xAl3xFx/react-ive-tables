66f52e6a8d3c7c4af1589900a377e203965f552e
Alex Petrov <alex.petrov@inerps.eu>
2026-02-17 17:47:23 +0200

    ReactiveTable -> Mobile filters button (header row) fix

--- src/ReactiveTable.tsx
+++ src/ReactiveTable.tsx
@@ -712,7 +712,7 @@ export const ReactiveTable = <T extends DataTableValue, K extends string>(
     }
 
     const getHeader = () => {
-        return <div className="export-buttons" style={{display: "flex", justifyContent: "space-between"}}>
+        return <div className="export-buttons" style={{display: "flex", gap: "1rem"}}>
             {(props.isMobile !== undefined && props.isMobile && props.mobileDataTemplate) ?
                 <Button icon={'pi pi-filter'} label={f({id: 'filters'})} className={"mb-3"}
                         onClick={() => setMobileFiltersDialogShown(true)}/> :
